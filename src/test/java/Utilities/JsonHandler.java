14ac36b8832e6c16ead8cc66ce753ed041e95874
Akzar A K, Mohammed <makzarak@deloitte.com>
2022-06-28 18:34:14 +0530

    Modified CommonAPIactions

--- src/test/java/Utilities/JsonHandler.java
+++ src/test/java/Utilities/JsonHandler.java
@@ -6,48 +6,45 @@ import io.restassured.response.Response;
 import org.json.simple.JSONObject;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class JsonHandler {
-    private static FileWriter jsonWrite;
 
     public static String readFileAsString(String file)throws Exception
     {
         return new String(Files.readAllBytes(Paths.get(file)));
     }
 
-    public static String editJsonFile(String jsonPath, String jsonValue,String jsonDataPath) throws IOException {
+    public static String editJsonValue(String jsonPath, String jsonValue, String jsonRequestBody)  {
         try {
-            String jsonString = readFileAsString(jsonDataPath);
-                jsonString= JsonPath.parse(jsonString).set(jsonPath, jsonValue).jsonString();
-            jsonWrite = new FileWriter(jsonPath);
-            jsonWrite.write(jsonString);
+            return JsonPath.parse(jsonRequestBody).set(jsonPath, jsonValue).jsonString();
         }
-        catch(Exception e) {
-// log the exception
-
-        }
-        finally {
-            jsonWrite.close();
-
+        catch (Exception e){
+            e.printStackTrace();
         }
         return null;
     }
 
-    public static String editJsonValue(String jsonPath, String jsonValue, String jsonRequestBody) {
-        String jsonString= JsonPath.parse(jsonRequestBody).set(jsonPath, jsonValue).jsonString();
-        return jsonString;
-    }
-
     public static String getJsonValue(Response response,String title) {
-        String jsonString= response.path(title).toString();
-        return jsonString;
+        try {
+            return response.path(title).toString();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static void jsonSchemaValidation(Response response, String jsonSchemaPath) {
-        response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(new File(jsonSchemaPath)));
+        try {
+            response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(new File(jsonSchemaPath)));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/api_tests/ApiCalls.java
+++ src/test/java/api_tests/ApiCalls.java
@@ -3,72 +3,111 @@ package api_tests;
 import Utilities.JsonHandler;
 import Utilities.PropertiesFileHandler;
 import Utilities.CommonAPIactions;
-import Utilities.StandardHeaders;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class ApiCalls extends BaseClass_Api {
     private static PropertiesFileHandler prop = new PropertiesFileHandler("config.properties");
     public static String jsonRequestBody;
-
-    public static String putUpdateUserPayload(String jsonDataFilePath) throws Exception {
+    public static String key="id";
+    public static String getPath="4";
+    public static String putPath="2";
+    public static String deletePath="3";
+    public static String putUpdateUserPayload(String jsonDataFilePath){
         try {
             jsonRequestBody = JsonHandler.readFileAsString(jsonDataFilePath);
             jsonRequestBody = JsonHandler.editJsonValue("$.name", "gokul", jsonRequestBody);
             jsonRequestBody = JsonHandler.editJsonValue("$.job", "dev", jsonRequestBody);
             return jsonRequestBody;
         }
-        catch (FileNotFoundException e){
+        catch (Exception e){
             e.printStackTrace();
         }
         return null;
     }
 
-    public static String postSingleUserPayload(String jsonDataFilePath) throws Exception {
+    public static String postSingleUserPayload(String jsonDataFilePath){
         try {
             jsonRequestBody = JsonHandler.readFileAsString(jsonDataFilePath);
             jsonRequestBody = JsonHandler.editJsonValue("$.name", "value", jsonRequestBody);
             jsonRequestBody = JsonHandler.editJsonValue("$.job", "testing", jsonRequestBody);
             return jsonRequestBody;
         }
-        catch (FileNotFoundException e){
+        catch (Exception e){
             e.printStackTrace();
         }
         return null;
     }
 
     @Test
-    public static void getSingleUserCall() throws IOException {
-        response = CommonAPIactions.getCall(CommonAPIactions.SINGLE_USER_API, StandardHeaders.AUTHENTICATION,"id");
-        CommonAPIactions.validateStatusCode(response,200);
-        JsonHandler.jsonSchemaValidation(response,prop.getProperty("GET_SINGLE_USER_SCHEMA"));
-        System.out.println("Get Call Executed");
+    public static void getSingleUserCall() {
+        try {
+            response = CommonAPIactions.getCall(CommonAPIactions.SINGLE_USER_API,key,getPath,null,null,null);
+            CommonAPIactions.validateStatusCode(response, 200);
+            JsonHandler.jsonSchemaValidation(response, prop.getProperty("GET_SINGLE_USER_SCHEMA"));
+            System.out.println("Get Call Executed "+ response.asString());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public static void getListUsersCall() {
+        try {
+            HashMap<String,String> m= new HashMap<>();
+            m.put("page","2");
+            response = CommonAPIactions.getCall(CommonAPIactions.LIST_USER_API,null,null,null,m,null);
+            CommonAPIactions.validateStatusCode(response, 200);
+            System.out.println("Get Call Executed "+response.asString());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public static void postCreateUserCall(){
+        try {
+            response = CommonAPIactions.postCall(CommonAPIactions.CREATE_USER_API, postSingleUserPayload(prop.getProperty("POSTJSONDATA")),null,null,null,null,null);
+            CommonAPIactions.validateStatusCode(response, 201);
+            JsonHandler.jsonSchemaValidation(response, prop.getProperty("POST_CREATE_USER_SCHEMA"));
+            validatePostSingleUserResponse(response);
+            System.out.println("Post Call Executed "+response.asString());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
     @Test
-    public static void postCreateUserCall() throws Exception {
-        response = CommonAPIactions.postCall(CommonAPIactions.CREATE_USER_API, postSingleUserPayload(prop.getProperty("POSTJSONDATA")), StandardHeaders.AUTHENTICATION,"id");
-        CommonAPIactions.validateStatusCode(response,201);
-        JsonHandler.jsonSchemaValidation(response,prop.getProperty("POST_CREATE_USER_SCHEMA"));
-        validatePostSingleUserResponse(response);
-        System.out.println("Post Call Executed");
+    public static void putUpdateUserCall() {
+        try {
+            response = CommonAPIactions.putCall(CommonAPIactions.UPDATE_USER_API, putUpdateUserPayload(prop.getProperty("POSTJSONDATA")),key,putPath,null,null,null);
+            CommonAPIactions.validateStatusCode(response, 200);
+            JsonHandler.jsonSchemaValidation(response, prop.getProperty("PUT_UPDATE_USER_SCHEMA"));
+            validatePutUpdateUserResponse(response);
+            System.out.println("Put Call Executed "+response.asString());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
     @Test
-    public static void putUpdateUserCall() throws Exception {
-        response = CommonAPIactions.putCall(CommonAPIactions.UPDATE_USER_API, putUpdateUserPayload(prop.getProperty("POSTJSONDATA")), StandardHeaders.AUTHENTICATION,"id");
-        CommonAPIactions.validateStatusCode(response,200);
-        JsonHandler.jsonSchemaValidation(response,prop.getProperty("PUT_UPDATE_USER_SCHEMA"));
-        validatePutUpdateUserResponse(response);
-        System.out.println("Put Call Executed");
-    }
-    @Test
-    public static void deleteUserCall() throws IOException {
-        response = CommonAPIactions.deleteCall(CommonAPIactions.DELETE_USER_API, StandardHeaders.AUTHENTICATION,"id");
-        CommonAPIactions.validateStatusCode(response,204);
-        System.out.println("Delete Call Executed");
+    public static void deleteUserCall(){
+        try {
+            response = CommonAPIactions.deleteCall(CommonAPIactions.DELETE_USER_API,key,deletePath,null,null,null);
+            CommonAPIactions.validateStatusCode(response, 204);
+            System.out.println("Delete Call Executed "+response.asString());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public static void validatePostSingleUserResponse(Response res){
